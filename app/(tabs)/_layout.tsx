52eca52fff9c23f5159fd780498f1efd663086a2
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2024-10-21 16:16:03 +0300

    added tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
-import { Tabs, Redirect } from 'expo-router';
-import {StatusBar} from "expo-status-bar";
-import {useTheme} from "@/components/Context/Context";
+import { Tabs } from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import { useTheme } from "@/components/Context/Context";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabsLayout = () => {
     const { theme, toggleTheme } = useTheme();
+
     return (
         <>
             <Tabs
@@ -17,25 +19,49 @@ const TabsLayout = () => {
                         borderTopColor: 'transparent',
                         height: 85,
                         position: 'absolute',
-                        zIndex: 0,  // Зменшений пріоритет для таб бара
+                        zIndex: 0,
                     },
                 }}
             >
                 <Tabs.Screen
                     name='Characters'
-                    options={{ title: 'Characters', headerShown: false, }}
+                    options={{
+                        title: 'Characters',
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="people-outline" color={color} size={size} />
+                        ),
+                    }}
                 />
                 <Tabs.Screen
                     name='Starships'
-                    options={{ title: 'Starships', headerShown: false, }}
+                    options={{
+                        title: 'Starships',
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="rocket-outline" color={color} size={size} />
+                        ),
+                    }}
                 />
                 <Tabs.Screen
                     name='Planets'
-                    options={{ title: 'Planets', headerShown: false, }}
+                    options={{
+                        title: 'Planets',
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="planet-outline" color={color} size={size} />
+                        ),
+                    }}
                 />
                 <Tabs.Screen
                     name='Films'
-                    options={{ title: 'Films', headerShown: false, }}
+                    options={{
+                        title: 'Films',
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="film-outline" color={color} size={size} />
+                        ),
+                    }}
                 />
             </Tabs>
             <StatusBar backgroundColor="#161622" style="light" />
